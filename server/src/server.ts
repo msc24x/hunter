a1cc7ebb5018375f0ed58224263fb560fe65c737
Maninderpal Singh <msc24x@gmail.com>
2022-01-26 15:22:16 +0530

    Add working authentication (session based) api endpoints

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,12 @@ import mysql, { Types } from 'mysql';
 import { AppDB } from './database/interface';
 import { RegisterRequest } from './database/types';
 import cookieParser from 'cookie-parser';
+import { Interface } from 'readline';
 const argon2 =  require('argon2');
+
+interface User{
+  id : string, email : string, name : string
+}
 
 const resCode = {
   serverErrror : 503,
@@ -13,7 +18,9 @@ const resCode = {
   accepted : 202,
   created : 201,
   badRequest : 400,
-  forbidden : 403
+  forbidden : 403,
+  notFound : 404,
+  found  : 302
 }
 
 const app = express();
@@ -68,19 +75,93 @@ app.get("/register", (req, res) =>{
           sendResponse(res, resCode.serverErrror);
           return
         }
-        sendResponse(res, resCode.created);
+        authenticate(req, res, (req : Request, res : Response, user : User)=>{
+          sendResponseJson(res, resCode.accepted, user);
+          console.log("Authentication successfull")
+        })
       });
     })
   })
 
 })
 
-function authenticate(req: Request, res : Response, callback : Function = ()=>{}) {
+app.get("/user", (req, res)=>{
+  getUser(req, res, (user : User)=>{
+    sendResponseJson(res, resCode.found, user);
+  })
+})
+
+app.get("/authenticate", (req, res)=>{
+
+  authenticate(req, res, (req : Request, res : Response, user : User)=>{
+    sendResponseJson(res, resCode.accepted, user);
+    console.log("Authentication successfull")
+  })
+
+})
+
+app.get("/logout", (req, res)=>{
+  console.log("logout requested")
+  const session_id = req.cookies.session_id
+  if(session_id){
+    dbConnection.query(` delete from session where session.id = "${session_id}" ; `, (err)=>{
+      if(err){
+        console.log(err)
+        sendResponse(res, resCode.serverErrror)
+        return
+      }
+      res = res.clearCookie("session_id")
+      sendResponse(res, resCode.success)
+    })
+  }else{
+    sendResponse(res, resCode.badRequest)
+  }
+})
+
+function getUser(req:Request, res : Response, callback : Function = (user : User)=>{}, user_id : string = "") {
+
+  let id = req.query.id
+  if(user_id != ""){
+    id = user_id
+  }
+  const email = req.query.email
+
+  if(id || email){
+    var query = ""
+    if(id)
+      query = ` select * from users where users.id = "${id}" ; `;
+    else if(email)
+      query = ` select * from users where users.email = "${email}" ; `
+
+    dbConnection.query(query, (err, rows)=>{
+      if(err){
+        console.log(err);
+        sendResponse(res, resCode.serverErrror);
+        return;
+      }
+      if(rows.length == 0){
+        sendResponse(res, resCode.notFound)
+        return;
+      }
+      callback({
+        id : rows[0].id,
+        email : rows[0].email,
+        name : rows[0].name
+      });
+    })
+  }else{
+    sendResponse(res, resCode.badRequest)
+  }
+}
+
+function authenticate(req: Request, res : Response, callback : Function = (req : Request, res : Response, user :  User)=>{}) {
 
   const email = req.query.email;
   const password = req.query.password;
+  const session_id = req.cookies.session_id
 
   if(email && password){
+
     dbConnection.query(` select * from users where users.email = "${email}";`, (err, rows)=>{
       if(err){
         console.log(err);
@@ -100,6 +181,7 @@ function authenticate(req: Request, res : Response, callback : Function = ()=>{}
           const session_id = randomBytes(12).toString("hex")+rows[0].id;
           dbConnection.query(` delete from session where session.user_id = "${rows[0].id}" ; `)
           if(req.query.remember == "true"){
+            console.log("remember me was "+req.query.remember)
             dbConnection.query(` insert into session(id, user_id) values( "${session_id}", "${rows[0].id}" ); `, err=>{
               if(err){
                 console.log(err);
@@ -107,13 +189,11 @@ function authenticate(req: Request, res : Response, callback : Function = ()=>{}
                 return
               }
               res.cookie("session_id", session_id);
-              sendResponse(res, resCode.accepted, );
-              callback()
+              callback(req, res, {id : rows[0].id, email : rows[0].email, name : rows[0].name})
             })
           }else{
             res =   res.clearCookie("remember")
-            sendResponse(res, resCode.accepted);
-            callback()
+            callback(req, res, {id : rows[0].id, email : rows[0].email, name : rows[0].name})
           }
         }
         else{
@@ -122,9 +202,7 @@ function authenticate(req: Request, res : Response, callback : Function = ()=>{}
       })
     })
   }
-  else if(req.cookies.session_id){
-    const session_id = req.cookies.session_id;
-
+  else if(session_id){
     dbConnection.query(` select * from session where session.id = "${session_id}";`, (err, rows)=>{
       if(err){
         console.log(err);
@@ -135,8 +213,11 @@ function authenticate(req: Request, res : Response, callback : Function = ()=>{}
         sendResponse(res, resCode.forbidden);
         return;
       }
-      sendResponse(res, resCode.accepted);
-      callback()
+
+      getUser(req, res, (user : User)=>{
+        callback(req, res, user);
+      },
+      rows[0].user_id)
     })
   }
   else{
@@ -145,16 +226,18 @@ function authenticate(req: Request, res : Response, callback : Function = ()=>{}
 
 }
 
-app.get("/authenticate", (req, res)=>{
-
-  authenticate(req, res, ()=>{
-    console.log("Authentication successfull")
-  })
-
-})
-
 function sendResponse(res : Response , code : number ,msg : string = "") {
+
   res.status(code).send(msg);
+}
+
+function sendResponseJson(res : Response , code : number , body : {
+  id : string,
+  email : string,
+  name : string,
+  msg? : string
+}) {
+  res.status(code).send(body);
 }
 
 
