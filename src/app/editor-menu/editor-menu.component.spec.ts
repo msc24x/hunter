9accad26c082c56d0171f704864423f0552ed18c
Maninderpal Singh <msc24x@gmail.com>
2022-02-15 13:26:05 +0530

    introduce editor menu showing all competitions the user created (overall WIP)

---
+++ src/app/editor-menu/editor-menu.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EditorMenuComponent } from './editor-menu.component';
+
+describe('EditorMenuComponent', () => {
+  let component: EditorMenuComponent;
+  let fixture: ComponentFixture<EditorMenuComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EditorMenuComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EditorMenuComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/editor-menu/editor-menu.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { userInfo } from 'os';
+import { CompetitionInfo, UserInfo } from 'server/src/database/types';
+import { AuthService } from '../services/auth/auth.service';
+import { CompetitionsDataService } from '../services/data/competitions-data.service';
+
+@Component({
+  selector: 'editor-menu',
+  templateUrl: './editor-menu.component.html',
+  styleUrls: ['./editor-menu.component.scss']
+})
+export class EditorMenuComponent implements OnInit {
+
+  isAuthenticated : boolean = false
+  user = {
+    id : "",
+    email : "",
+    name : ""
+  }
+
+  userCompetitions : Array<CompetitionInfo> | null = null
+
+
+  constructor(
+    private authService : AuthService,
+    private competitionsDataService : CompetitionsDataService
+  ) {
+
+    this.authService.isAuthenticated.subscribe(isAuth=>{
+      this.user = this.authService.user
+      this.isAuthenticated = isAuth;
+    })
+
+  }
+
+  ngOnInit(): void {
+
+    this.authService.authenticate_credentials().subscribe(res=>{
+      if(res.status == 202){
+
+        // save user info
+        const body = res.body as UserInfo
+        this.user = {
+          id :  body.id,
+          email : body.email,
+          name : body.name
+        }
+
+        // save user info for all components subscribed to the service
+        this.authService.user = this.user
+        this.isAuthenticated = true
+        this.authService.isAuthenticated.next(true)
+
+        // fetch user's all created competitions
+        this.competitionsDataService.getPublicCompetitions({
+          public : false,
+          host_user_id : this.user.id
+        }).subscribe(res=>{
+          this.userCompetitions = res.body
+        },
+        err=>{
+          this.userCompetitions = err.error
+        })
+
+      }
+    })
+
+  }
+
+}
