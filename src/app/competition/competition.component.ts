ed0125023a30d5a808cc0f4d8be692c7509a40be
Maninderpal Singh <msc24x@gmail.com>
2022-02-26 16:20:41 +0530

    Initial competition participation component

--- src/app/competition/competition.component.ts
+++ src/app/competition/competition.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-
+import { ActivatedRoute, ActivationEnd } from '@angular/router';
+import * as ace from 'ace-builds';
 @Component({
   selector: 'competition',
   templateUrl: './competition.component.html',
@@ -9,15 +9,32 @@ import { ActivatedRoute } from '@angular/router';
 export class CompetitionComponent implements OnInit {
 
   c_id : string = "";
+  editor!: ace.Ace.Editor;
 
   constructor(private route : ActivatedRoute) {
     const idParam = route.snapshot.paramMap.get("competition_id");
     if(idParam){
       this.c_id = idParam;
     }
+
   }
 
+
   ngOnInit(): void {
+    this.initEditor()
+  }
+
+
+  initEditor(){
+    this.editor = ace.edit("editor")
+    ace.config.set("basePath", "https://unpkg.com/ace-builds@1.4.12/src-noconflict")
+    /**
+     * twilight
+     * monokai
+     * terminal
+     */
+    this.editor.setTheme("ace/theme/twilight")
+    this.editor.session.setMode("ace/mode/python")
   }
 
 }
