98754a98d2ba941740cc36e4b69d66f40570b0e6
Maninderpal Singh <msc24x@gmail.com>
2022-02-26 16:35:09 +0530

    batch script to copy and execute judge (tester.py) inside docker container

---
+++ server/src/runTests.bat
@@ -0,0 +1,10 @@
+
+set fileNumber = %1
+set fileType = %2
+
+
+docker cp "src/database/files/c"+$fileNumber+"."+$fileType "acpjs:/app/code.%fileType%
+docker cp "src/database/files/%fileNumber%i.txt" "acpjs:/app/files/input.txt"
+docker cp "src/database/files/%fileNumber%o.txt" "acpjs:/app/files/ans.txt"
+
+docker exec -i acpjs python3 app/tester.py "%fileType%"
