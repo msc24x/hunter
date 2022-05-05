084065a081c32a762cdb2279bcfb52f724616edd
Maninderpal Singh <msc24x@gmail.com>
2022-05-05 12:31:10 +0530

    Prepare for efficient server infrastructure; Singleton dbConnection provider

---
+++ server/src/database/database.ts
@@ -0,0 +1,41 @@
+import { MysqlError } from "mysql";
+import { Connection } from "mysql";
+import mysql, {Types} from 'mysql'
+import { stat } from "fs";
+
+export class database{
+  private static _dbConnection? : Connection
+  private static inititalized = false
+  private static lock = false
+
+  static isInitialized = ()=>{return this.inititalized}
+
+  static getDataBase() : mysql.Connection {
+
+    while(database.lock){}
+    database.lock = true
+
+
+    if(database.inititalized && database._dbConnection)
+      return database._dbConnection
+    database._dbConnection = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "password",
+      database : "hunter_db"
+    })
+    database.inititalized = true
+
+
+    database.lock = false
+
+    database._dbConnection.connect(err=>{
+      if(err){
+        console.log(err);
+        return
+      }
+    });
+
+    return database._dbConnection
+  }
+}
