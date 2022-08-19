648b6bb0eab4fcf467a265f5a89491eb28c82f51
msc24x <msc24x@gmail.com>
2022-08-19 20:26:54 +0530

    Fix tester.py and gateway batch script to work with new features

--- server/src/dockerApp/tester.py
+++ server/src/dockerApp/tester.py
@@ -3,14 +3,18 @@ from fileinput import filename
 import sys
 import os
 
-if len(sys.argv) < 2:
+
+samples = 'false'
+
+if len(sys.argv) < 3:
     print('ERROR : language not specified, supported_langs = ["c", "cpp", "py", "js"]')
     exit()
 
 def postResult(code, message = ""):
-    print(code , " - " , codeMsg[code])
-    print(message)
-    os.system("truncate -s 0 "+codeOutputfile)
+  print(code , " - " , codeMsg[code])
+  print(message)
+  os.system("truncate -s 0 "+codeOutputfile)
+  exit()
 
 
 supported_langs = ["c", "cpp", "py", "js"]
@@ -21,12 +25,15 @@ exefile = "app/build/code"
 codeOutputfile = "app/files/output.txt"
 codeInputfile = "app/files/input.txt"
 resultFile = "app/res.txt"
-language = (sys.argv[1])
+language = sys.argv[1]
+samples = sys.argv[2]
+
 codeMsg = {
   1 : "Accepted",
   2 : "Wrong Answer",
-  3 : "Runtime Error",
-  4 : "Timeout"
+  3 : "Compilation Error",
+  4 : "Timeout",
+  5 : "Runtime Error"
 }
 
 if language not in supported_langs :
@@ -41,48 +48,57 @@ if not os.path.isfile(filename):
   exit()
 
 if language == "c":
-  compilationCode = os.system("gcc " + filename + " -o "+exefile + " > " + codeOutputfile)
+  compilationCode = os.system("gcc " + filename + " -o "+exefile + " 2> " + codeOutputfile)
   if compilationCode == 0:
-      processCode = os.system( "timeout 5 ./" + exefile + " < "+ codeInputfile + " > " + codeOutputfile)
+      processCode = os.system( "timeout 5 ./" + exefile + " < "+ codeInputfile + " 2>> " + codeOutputfile+ " 1>> " + codeOutputfile)
 
 elif language == "cpp":
-  compilationCode = os.system("g++ " + filename + " -o "+exefile + " > " + codeOutputfile)
+  compilationCode = os.system("g++ " + filename + " -o "+exefile + " 2> " + codeOutputfile)
   if compilationCode == 0:
-      processCode = os.system( "timeout 5 ./" + exefile + " < "+ codeInputfile + " > " + codeOutputfile)
+      processCode = os.system( "timeout 5 ./" + exefile + " < "+ codeInputfile  + " 2>> " + codeOutputfile+ " 1>> " + codeOutputfile)
 
 elif language == "py":
-    processCode = os.system( "timeout 5 python3 " + filename + " < "+ codeInputfile + " > " + codeOutputfile)
+    processCode = os.system( "timeout 5 python3 " + filename + " < "+ codeInputfile  + " 2>> " + codeOutputfile+ " 1>> " + codeOutputfile)
 
 elif language == "js":
-  processCode = os.system( "timeout 5 node " + filename + " < "+ codeInputfile + " > " + codeOutputfile )
+  processCode = os.system( "timeout 5 node " + filename + " < "+ codeInputfile  + " 2>> " + codeOutputfile+ " 1>> " + codeOutputfile)
 
 codeOutput = open(codeOutputfile, "r")
-codeOutput = codeOutput.readlines()
+codeOutputLines = codeOutput.readlines()
+codeOutput.seek(0,0)
 correctOutput = open("app/files/ans.txt", "r")
+correctOutputLines = correctOutput.readlines()
+correctOutput.seek(0,0)
 
 # Error while compilation
 if compilationCode > 0:
-  postResult(3, message = "".join(codeOutput))
-  exit()
+  postResult(3, message = "".join(codeOutputLines))
+  
 # Runtime error or timeout on 5sec
 elif processCode == 15:
   postResult(4, message = "")
-  exit()
+  
 elif processCode > 0:
-  postResult(3, message = "".join(codeOutput))
-  exit()
+  postResult(5, message = "".join(codeOutputLines))
+  
 
 
-for line in codeOutput:
-  cLine = correctOutput.readline()
+for cLine in correctOutputLines:
+  line = codeOutput.readline()
 
-  line = line.replace("", "")
-  cLine = cLine.replace("", "")
+  line = line.replace("\n", "")
+  cLine = cLine.replace("\n", "")
 
   if line != cLine:
-      postResult(2, message = "".join(codeOutput))
-      exit()
+    if samples == 'true':
+      postResult(2, message = "".join(codeOutputLines))
+    else:
+      postResult(2, message = "")
+      
+    
+if samples == 'true':
+  postResult(1, message = "".join(codeOutputLines))
+else:
+  postResult(1, message = "")
 
-postResult(1, message = "".join(codeOutput))
-exit()
 
--- server/src/scirpts/runTests.bat
+++ server/src/scirpts/runTests.bat
@@ -1,10 +1,26 @@
 @echo off
 set fileNumber=%1
 set fileType=%2
+set samples=%3
+set user_id=%4
+set tests=%5
+set sols=%6
 
 
-docker cp "src/database/files/%fileNumber%.%fileType%" "elated_yalow:/app/code.%fileType%"
-docker cp "src/database/files/%fileNumber%_t.txt" "elated_yalow:/app/files/input.txt"
-docker cp "src/database/files/%fileNumber%_s.txt" "elated_yalow:/app/files/ans.txt"
+docker cp "src/database/files/%fileNumber%_%user_id%.%fileType%" "elated_yalow:/app/code.%fileType%"
 
-docker exec -i elated_yalow python3 app/tester.py %fileType%
+if %samples%==true (
+    printf %tests% > temp.txt
+    docker cp temp.txt elated_yalow:/app/files/input.txt
+) else (
+    docker cp "src/database/files/%fileNumber%_t.txt" "elated_yalow:/app/files/input.txt"
+)
+
+if %samples%==true (
+    printf %sols% > temp.txt
+    docker cp temp.txt elated_yalow:/app/files/ans.txt
+) else (
+    docker cp "src/database/files/%fileNumber%_s.txt" "elated_yalow:/app/files/ans.txt"
+)
+
+docker exec -i elated_yalow python3 app/tester.py %fileType% %samples%
