09c4fcb0ad29902539e0cc72b54d0db2d33e4142
msc24x <msc24x@gmail.com>
2022-08-25 16:36:33 +0530

    Add experimental sendEmail utility

--- server/src/util/util.ts
+++ server/src/util/util.ts
@@ -1,8 +1,30 @@
-import { CompetitionInfo, HunterExecutable } from "../environments/environment";
+import { CompetitionInfo, HunterExecutable, UserInfo } from "../environments/environment";
+import Mailgun from "mailgun-js";
+
 
 export class Util {
 
     
+    static sendEmail(userInfo : UserInfo){
+        const DOMAIN = 'sandbox511079e5185343208301c8c7d6c486fd.mailgun.org';
+
+        const mg = Mailgun({apiKey: "1fe01efb475905bebf66ecb790ffd085-c76388c3-624a0a92", domain: DOMAIN});
+        const data = {
+            from: 'Hunter <creator@hunter.tech>',
+            to: userInfo.email,
+            subject: 'Email verification',
+            text: `Hi, ${userInfo.name}! Thank you for joining us at Hunter. \n\n Here is the verification code - <testing>.\n\nIf it was not you who requested this code, kindly ignore this email.\n Thank you!`
+        };
+
+        mg.messages().send(data, function (error, body) {
+            if(error){
+                console.log(error)
+                return
+            }
+            console.log(body);
+        });
+    }
+
     static sendResponse(res : any , code : number ,msg : string = "") {
 
         res.status(code).send(msg);
