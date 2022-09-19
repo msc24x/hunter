93bff00493cbbe12aabc5034e11974f258de6459
msc24x <msc24x@gmail.com>
2022-09-19 14:25:29 +0530

    Introduce Judge and scoreboard services

---
+++ server/src/services/scoreboardService.ts
@@ -0,0 +1,58 @@
+import { Service } from "typedi";
+import models from "../container/models";
+import { User } from "../database/models/User";
+import { HunterExecutable, UserInfo } from "../environments/environment";
+
+
+@Service()
+export class ScoreboardService{
+
+    async updateResult(user : UserInfo, hunterExecutable : HunterExecutable, accepted : boolean, points : number){
+        
+        models.results.findAll({
+              user_id : user.id,
+              question_id : hunterExecutable.for.question_id,
+              competition_id : hunterExecutable.for.competition_id,
+            },
+
+            (rows, err)=>{
+              if(err){
+                console.log(err)
+                return
+              }
+
+              let pts = accepted ? points : 0
+        console.log("new result ")
+            
+              if(rows.length == 0){
+                models.results.post( 
+                  {
+                    user_id : user.id,
+                    question_id : hunterExecutable.for.question_id,
+                    competition_id : hunterExecutable.for.competition_id,
+                    result : pts
+                  },
+                  err=>{ 
+                    if(err){
+                      console.log(err)
+                      return
+                    }
+                  }
+                )
+              }
+              else{
+                if(rows[0].result == '0'){
+                    
+                    models.results.update(rows[0].id, pts+"", err=>{
+                        if(err){
+                            console.log(err)
+                            return
+                        }
+                    })
+                }
+              }
+            }
+        )
+    }
+
+}
