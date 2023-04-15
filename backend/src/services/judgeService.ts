f42bb89bba803db88aa0133bf3f69f48ac9d067b
Maninderpal Singh <msc24x@gmail.com>
2023-04-15 18:33:31 +0530

    fix: debug and refactor judgeService

--- backend/src/services/judgeService.ts
+++ backend/src/services/judgeService.ts
@@ -3,11 +3,23 @@ import { writeFile } from 'fs';
 import { Service } from 'typedi';
 import { HunterExecutable, QuestionInfo, UserInfo } from '../config/types';
 import { Util } from '../util/util';
+import  config  from '../config/config';
 
 @Service({ global: true })
 export class JudgeService {
 	private isProcessingUserMap = new Map<string, boolean>();
 	private entries = 0;
+	private gatewayPath: string;
+	private filesPath: string = "src/database/files/";
+
+	constructor() {
+		if (config.env == "prod") {
+			this.gatewayPath = "src/scripts/linux/init_judge.sh"
+		}
+		else {
+			this.gatewayPath = "src/scripts/win/init_judge.bat"
+		}
+	}
 
 	private saveEntry({ id }: UserInfo) {
 		this.isProcessingUserMap.set(id, true);
@@ -17,6 +29,32 @@ export class JudgeService {
 	private remEntry({ id }: UserInfo) {
 		this.isProcessingUserMap.set(id, false);
 		this.entries--;
+	}
+
+	private _cmd(
+		hunterExecutable: HunterExecutable,
+		samples: Boolean,
+		question: QuestionInfo,
+		user: UserInfo
+	) {
+		return `${this.gatewayPath} ${Util.getFileName(
+				hunterExecutable
+			)} ${hunterExecutable.solution.lang} ${samples} ${
+				user.id
+			} "${question.sample_cases.replace(
+				'\n',
+				'\\n'
+			)}" "${question.sample_sols.replace('\n', '\\n')}"`
+			
+	}
+
+	private buildFilePath(
+		hunterExecutable: HunterExecutable,
+		user : UserInfo
+	) {
+		return `${this.filesPath}${Util.getFileName(hunterExecutable)}_${
+			user.id
+		}.${hunterExecutable.solution.lang}`
 	}
 
 	getNumberOfEntries = () => {
@@ -35,32 +73,28 @@ export class JudgeService {
 			else this.saveEntry(user);
 
 			writeFile(
-				`src/database/files/${Util.getFileName(hunterExecutable)}_${
-					user.id
-				}.${hunterExecutable.solution.lang}`,
-				`${hunterExecutable.solution.code}`,
+				this.buildFilePath(hunterExecutable, user),
+				hunterExecutable.solution.code,
 				{ flag: 'w' },
 				(err) => {
 					if (err) {
-						reject(err);
 						this.remEntry(user);
+						reject(err);
 					}
 
-					exec(
-						`src/scripts/runTests.bat ${Util.getFileName(
-							hunterExecutable
-						)} ${hunterExecutable.solution.lang} ${samples} ${
-							user.id
-						} "${question.sample_cases.replace(
-							'\n',
-							'\\n'
-						)}" "${question.sample_sols.replace('\n', '\\n')}"`,
+					const cmd = this._cmd(
+						hunterExecutable,
+						samples,
+						question,
+						user
+					);
+
+					exec(cmd,
 						(error, stdout, stderr) => {
 							if (error) {
-								reject(err);
 								this.remEntry(user);
+								reject(err);
 							}
-
 							this.remEntry(user);
 							resolve(stdout);
 						}
