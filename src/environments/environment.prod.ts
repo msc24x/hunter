cc6042729c2a1b0319b82bdd0a45a595a5a54f2a
Maninderpal Singh <msc24x@gmail.com>
2023-05-27 17:50:08 +0530

    chore: lighthouse optimizations

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,7 +8,7 @@ export const domainName = "hunter.cambo.in"
 export const environment = {
   production: true,
   apiUrl: `${protocol}://${domainName}/api`,
-  version : "v1.9.7"
+  version : "v1.9.8"
 };
 
 export interface UserInfo{
